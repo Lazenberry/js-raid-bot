5d6be23656a95248a8f7a7dd1a2fc11855b12f01
ltBurton <74010151+ltBurton@users.noreply.github.com>
2024-08-16 19:10:38 +0100

    initial

--- entry.js
+++ entry.js
@@ -1,4 +1,19 @@
 require("module-alias/register")
 require("dotenv").config()
 
+//!Discord Client
+
+const Discord = require("discord.js")
+
+const client = new Discord.Client({
+    intents: [
+        "MessageContent"
+    ]
+})
+
+client.on("ready", () => {
+    console.log("Bot Started");
+})
+
+client.login(process.env.bot_token)
 
